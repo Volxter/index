083af644fe1b56ae2cb67373760df1e468771873
Volxter <155483160+Volxter@users.noreply.github.com>
2024-01-02 16:53:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <section>
+            <div id="main-login">
+                <!--Imagem -->
+
+                <!--Fim imagem-->
+                <div id="menu">
+
+                <div id="card-login">
+                        
+                    <h1>ENTRAR</h1>
+                    <div class="textfield"><!--EMAIL-->
+                        <label form="EMAIL">E-mail</label>
+                        <input type="email" placeholder="E-mail:">
+                        </div>
+                    <div class="textfield"><!--Senha-->
+                        <label form="Senha">Senha</label>
+                        <input type="password" placeholder="Senha:">
+                    </div>
+                    <button>
+                        <span><a id="entrar"  href="pagina.html">ENTRAR</a></span>
+                      </button>
+
+                    <a href="">Criar Cadastro</a>
+                </div>
+                
+            </div>
+            
+            </div>
+        </section>
+       
+</html>
